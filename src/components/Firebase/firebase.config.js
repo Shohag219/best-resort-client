4558da2cee21c0f8f5fbf499ce2abeac9613175d
mahmudul <mahmudulhasanshohag219@gmail.com>
2021-12-18 23:40:57 +0600

    okk

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyADuRcKjSbFzQNEiwySzBfpyll3O4N2svk",
+  authDomain: "world-best-resort.firebaseapp.com",
+  projectId: "world-best-resort",
+  storageBucket: "world-best-resort.appspot.com",
+  messagingSenderId: "493129222275",
+  appId: "1:493129222275:web:e8c8d80cb41c617067f3b6",
+};
+export default firebaseConfig;
